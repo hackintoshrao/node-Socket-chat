ad7ef394b5c54622f5b8fe781c1b9ce9686cce2a
Karthic Rao <kartronics85@gmail.com>
2013-10-04 21:57:49 +0530

    Code working properly . less is not used . REquire.js will be setup since the chromes javascript console showed issues because of chat.js loading before than jquery , so to manage dependency require.js will be used

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,12 +1,13 @@
-var socket = io.connect("/");//get connected to the server 
+var socket = io.connect('/');//get connected to the server 
 
 socket.on("message",function(data){ //associate an event handler for the messages from the server 
+	data=JSON.parse(data);
 	$('#messages').append('<div class="'+data.type'">' + data.message + '</div>' ); //append the messages to div messages space on the page .
 
 });
 
-$(function{
-	$('#send').click(function(){
+$(function(){
+	$('#send').click(function(data){
 		var data = {
 			type:'userMessage',
 			message:$('#message').val() 
