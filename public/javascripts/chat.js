bc22d7362918793c31a3f6366b9a40cf880af2aa
Karthic Rao <kartronics85@gmail.com>
2013-10-04 08:11:51 +0530

    The client side code chat.js added to /public/javascripts folder

---
+++ public/javascripts/chat.js
@@ -0,0 +1,17 @@
+var socket = io.connect("/");//get connected to the server 
+
+socket.on("message",function(data){ //associate an event handler for the messages from the server 
+	$('#messages').append('<div class="'+data.type'">' + data.message + '</div>' ); //append the messages to div messages space on the page .
+
+});
+
+$(function{
+	$('#send').click(function(){
+		var data = {
+			type:'userMessage',
+			message:$('#message').val() 
+		};
+		socket.send(JSON.stringify(data));
+		$('#message').val('');
+	});
+});
