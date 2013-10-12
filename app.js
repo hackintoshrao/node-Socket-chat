de7a42356736feb5ed3fae8b281ea3aadca85691
Karthic Rao <kartronics85@gmail.com>
2013-10-12 08:13:16 +0530

    index and layout changed in the process of debugginh the application

--- app.js
+++ app.js
@@ -19,11 +19,6 @@ app.configure(function(){
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
-  app.use(require('less-middleware')({
-    src:__dirname+'/public',
-    compress:true
-
-  }));
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
 });
