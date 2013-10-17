123f1e821a07098e3dd1ff23ec9fcb6f4f4aa247
Karthic Rao <kartronics85@gmail.com>
2013-10-18 00:17:54 +0530

    small error in the chat.js was stopping the system from working , noticed that the error was in the message handler of the client in the first few lines , doubt that the issue is due to DOM elements not loading before execution

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -2,8 +2,8 @@ var socket = io.connect('/');//get connected to the server
 
 socket.on("message",function(data){ //associate an event handler for the messages from the server 
 	data=JSON.parse(data);
-	$('#messages').append('<div class="'+data.type'">' + data.message + '</div>' ); //append the messages to div messages space on the page .
-
+	var msg = document.getElementById('messages');
+	msg.innerHTML+=data.message;
 });
 
 $(function(){
